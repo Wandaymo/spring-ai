5b9f389e2acb52c4fa5490f8ff927a2dae000aa3
Wandaymo <wandaymo@hotmail.com>
2026-02-11 14:44:47 -0300

    Added a simple prompt controller

---
+++ src/main/java/com/wandaymo/springai/SimplePromptController.java
@@ -0,0 +1,24 @@
+package com.wandaymo.springai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimplePromptController {
+
+    private final ChatClient chatClient;
+
+    public SimplePromptController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping("/")
+    public String simple(@RequestParam(value = "message") String message) {
+        ChatResponse chatResponse = chatClient.prompt().user(message).call().chatResponse();
+        return chatResponse.toString();
+
+    }
+}
